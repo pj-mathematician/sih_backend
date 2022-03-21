2ae397d423a42e2535dd484d283767e0ce7494fd
Poojan Vachharajani <pjmathematician@gmail.com>
2022-03-21 23:23:22 +0530

    added images

--- data/SIH Image Stash/SIH Image Stash/data_maker.py
+++ data/SIH Image Stash/SIH Image Stash/data_maker.py
@@ -1,18 +1,19 @@
 import pyimgur
 import os
-CLIENT_ID = "18249260aac26ae"
+CLIENT_ID = "00f3771c2e933ea"
 im = pyimgur.Imgur(CLIENT_ID)
 # links = []
 data = {}
 l = ['Cars', 'Indian Food', 'Pets']
+iter = 0
 for i in l:
     data[i] = {}
     m = os.listdir(i)
     for j in m:
-        data[i][j] = []
+        data[i][j] = {}
         for k in os.listdir(i+'/'+j):
             print(i + '/' + j + '/' + k)
-            data[i][j].append(im.upload_image(i+'/'+j+'/'+k).link)
+            data[i][j]["image_{}".format(iter)] = im.upload_image(i + '/' + j + '/' + k).link
 with open('data.txt', 'w') as f:
     f.write(str(data))
 
