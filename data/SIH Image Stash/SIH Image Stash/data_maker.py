cd1dce49d28cce244cdbc2bf0bd8ce258585c191
Poojan Vachharajani <pjmathematician@gmail.com>
2022-03-21 22:50:46 +0530

    added images

--- data/SIH Image Stash/SIH Image Stash/data_maker.py
+++ data/SIH Image Stash/SIH Image Stash/data_maker.py
@@ -8,15 +8,11 @@ l = ['Cars', 'Indian Food', 'Pets']
 for i in l:
     data[i] = {}
     m = os.listdir(i)
-    links = []
     for j in m:
         data[i][j] = []
-        links = []
         for k in os.listdir(i+'/'+j):
             print(i + '/' + j + '/' + k)
-            links.append(im.upload_image(i+'/'+j+'/'+k).link)
-            data[i][j].extend(links)
-print(data)
+            data[i][j].append(im.upload_image(i+'/'+j+'/'+k).link)
 with open('data.txt', 'w') as f:
     f.write(str(data))
 
