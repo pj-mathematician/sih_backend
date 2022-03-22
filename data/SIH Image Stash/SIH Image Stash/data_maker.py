63f9437d0a62661a9300bac67692679c6dbd15d0
Poojan Vachharajani <pjmathematician@gmail.com>
2022-03-22 11:13:52 +0530

    added images

--- data/SIH Image Stash/SIH Image Stash/data_maker.py
+++ data/SIH Image Stash/SIH Image Stash/data_maker.py
@@ -6,15 +6,22 @@ im = pyimgur.Imgur(CLIENT_ID)
 data = {}
 l = ['Cars', 'Indian Food', 'Pets']
 iter = 0
+flag = False
 for i in l:
     data[i] = {}
     m = os.listdir(i)
     for j in m:
         data[i][j] = {}
+        if j == "dogs":
+            flag = True
+        if not flag:
+            continue
         for k in os.listdir(i+'/'+j):
             print(i + '/' + j + '/' + k)
             data[i][j]["image_{}".format(iter)] = im.upload_image(i + '/' + j + '/' + k).link
             iter += 1
-with open('data.txt', 'w') as f:
+            with open('data4.txt', 'w') as f:
+                f.write(str(data))
+with open('data4.txt', 'w') as f:
     f.write(str(data))
 
---
+++ data/SIH Image Stash/SIH Image Stash/romantoint.py
@@ -0,0 +1,43 @@
+emplist = [
+    ['A', 'Delhi', '1 Jan 2000', '5000'],
+    ['B', 'Kolkata', '2 Jan 2000', '10000'],
+    ['C', 'Mumbai', '3 Jan 2000', '15000'],
+    ['D', 'Chennai', '4 Jan 2000', '20000'],
+    ['E', 'Bangalore', '5 Jan 2000', '25000'],
+    ['F', 'Hyderabad', '6 Jan 2000', '30000'],
+    ['G', 'Ahmedabad', '7 Jan 2000', '35000'],
+]
+
+# make a set empset with subsets containing values from emplist
+empset = {
+    {'A', 'Delhi', '1 Jan 2000', '5000'},
+    {'B', 'Kolkata', '2 Jan 2000', '10000'},
+    {'C', 'Mumbai', '3 Jan 2000', '15000'},
+    {'D', 'Chennai', '4 Jan 2000', '20000'},
+    {'E', 'Bangalore', '5 Jan 2000', '25000'},
+    {'F', 'Hyderabad', '6 Jan 2000', '30000'},
+    {'G', 'Ahmedabad', '7 Jan 2000', '35000'}
+}
+
+# make a tuple emptuple with subsets containing values from emplist
+emptuple = (
+    ('A', 'Delhi', '1 Jan 2000', '5000'),
+    ('B', 'Kolkata', '2 Jan 2000', '10000'),
+    ('C', 'Mumbai', '3 Jan 2000', '15000'),
+    ('D', 'Chennai', '4 Jan 2000', '20000'),
+    ('E', 'Bangalore', '5 Jan 2000', '25000'),
+    ('F', 'Hyderabad', '6 Jan 2000', '30000'),
+    ('G', 'Ahmedabad', '7 Jan 2000', '35000')
+)
+
+# make a dictionary empdict with subsets containing values from emplist
+empdict = {
+    'A': {'Delhi', '1 Jan 2000', '5000'},
+    'B': {'Kolkata', '2 Jan 2000', '10000'},
+    'C': {'Mumbai', '3 Jan 2000', '15000'},
+    'D': {'Chennai', '4 Jan 2000', '20000'},
+    'E': {'Bangalore', '5 Jan 2000', '25000'},
+    'F': {'Hyderabad', '6 Jan 2000', '30000'},
+    'G': {'Ahmedabad', '7 Jan 2000', '35000'}
+}
+
